bf2fd2052db00f8a4874c9e61386ac6432885ec0
arifinoid <rohmadarifin007@gmail.com>
2025-05-12 22:01:41 +0700

    feat: add app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [] do
+    member do
+      post 'follow'
+      delete 'unfollow'
+      get 'followings/sleep_records', to: 'users#followings_sleep_records'
+    end
+  end
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :sleep_records, only: [:create, :index]
 end
