b194387e76c8089829997af82e6fd338748a724d
arifinoid <rohmadarifin007@gmail.com>
2025-05-12 21:36:17 +0700

    feat: add follow model and adjust user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
+
+  has_many :active_follows, class_name: 'Follow', foreign_key: :follower_id
+  has_many :followings, through: :active_follows, source: :followed
+
+  has_many :passive_follows, class_name: 'Follow', foreign_key: :followed_id
+  hass_many :followers, through: :passive_follows, source: :follower
 end
